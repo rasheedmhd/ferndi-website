65cbfed14f4c6a02538c4de9bdba55e32afdc0e9
rasheedmhd <starletgh@gmail.com>
2024-04-06 21:02:07 +0000

    tailwind new site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heroBlue:           "#1e3a8a",
+        borderGrey:         "#C1C1C1",
+        ctaGrey:            "#403E3E",
+        textGrey:           "#383838",
+        iconGreen:          "#5BD422",
+        yellowBg:           "#FCF2CF",
+        cardOrange:         "#FB8543",
+        cardTeal:           "#2CADF5",
+        cardBeige:          "#FAEFD3",
+        cardWebTeal:        "#29B3D2",
+        webTeal:            "#00A3FF",
+        cardGreen:          "#B5FE93",
+        textGreen:          "#14532d",
+        cardPurple:         "#7000FF",
+        typeGreen:          "#A6D229",
+        devCongressPink:    "#EC008B",
+      },
+    },
   },
   plugins: [],
 }
